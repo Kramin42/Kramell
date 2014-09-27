48d9fdb97d2dd45b5ea9edfaa28cd240c65afce3
Cameron <kramin42@gmail.com>
2014-09-27 16:02:46 +1200

    add some debug logging

--- server.js
+++ server.js
@@ -198,8 +198,8 @@ function init() {
                             if (ch=='##csdc' && csdcrunning) {
                                 for (var csdcwk in csdcdata) {
                                     if (csdcdata.hasOwnProperty(csdcwk)){
-                                        console.log("checking for week "+csdcwk);
-                                        console.log("active: "+csdcdata[csdcwk]["active"]+", char match: "+message.match("\(L\d+ "+csdcdata[csdcwk]["wkchar"]+"\)"));
+                                        console.log("checking for char "+csdcdata[csdcwk]["wkchar"]);
+                                        console.log("active: "+csdcdata[csdcwk]["active"]+", char match: "+message.match(new RegExp("\(L\d+ "+csdcdata[csdcwk]["wkchar"]+"\)")));
                                         if (csdcdata[csdcwk]["active"] && message.search("\(L\d+ "+csdcdata[csdcwk]["wkchar"]+"\)")>-1){
                                             console.log("checking points for "+name);
                                             check_csdc_points(bot, name, message, csdcwk);
