c6c7e29c625d9df507aa08150122eac17ebce9a7
Cameron <kramin42@gmail.com>
2014-09-30 00:26:08 +1300

    testing

--- server.js
+++ server.js
@@ -288,7 +288,7 @@ function do_command(arg) {
                 toinsert = {}
                 toinsert["colourmap."+argfilter] = argcolour
                 //console.log("adding "+toinsert);
-                db.channels.update({"channel":argchan},{$add: toinsert});
+                db.channels.update({"channel":argchan},{$set: toinsert});
             }
         } else if (arg.length==2) {
             db.channels.distinct('colourmap', {'channel':arg[1]}, function(err, colourmap) {
