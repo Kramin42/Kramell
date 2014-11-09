1210882eaaeb9749e01be1eea4ea8e1606281c1f
Cameron <kramin42@gmail.com>
2014-11-10 12:17:45 +1300

    new function for counting logfile fetch bytes

--- server.js
+++ server.js
@@ -69,6 +69,72 @@ function byteCount(s) {
     return encodeURI(s).split(/%..|./).length - 1;
 }
 
+/**
+ * Function to fix native charCodeAt()
+ *
+ * Now, we can use fixedCharCodeAt("foo€", 3); for multibyte (non-bmp) chars too.
+ *
+ * @access public
+ * @see https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/String/charCodeAt
+ * @note If you hit a non-bmp surrogate, the function will return false
+ * @param str String Mixed string to get charcodes
+ * @param idx Integer Position of the char to get
+ * @return code Integer Result charCodeAt();
+ */
+function fixedCharCodeAt(str, idx) {
+    idx = idx || 0;
+    var code = str.charCodeAt(idx);
+    var hi, low;
+    if (0xD800 <= code && code <= 0xDBFF) { // High surrogate (could change last hex to 0xDB7F to treat high private surrogates as single characters)
+        hi = code;
+        low = str.charCodeAt(idx + 1);
+        if (isNaN(low)) {
+            throw 'Kein gültiges Schriftzeichen oder Speicherfehler!';
+        }
+        return ((hi - 0xD800) * 0x400) + (low - 0xDC00) + 0x10000;
+    }
+    if (0xDC00 <= code && code <= 0xDFFF) { // Low surrogate
+        // We return false to allow loops to skip this iteration since should have already handled high surrogate above in the previous iteration
+        return false;
+        /*hi = str.charCodeAt(idx-1);
+         low = code;
+         return ((hi - 0xD800) * 0x400) + (low - 0xDC00) + 0x10000;*/
+    }
+    return code;
+}
+ 
+/**
+ * Gets size of a UTF-8 string in bytes
+ *
+ * @autor Frank Neff <fneff89@gmail.com>
+ * @license GPL v2
+ * @access public
+ * @param str String Input string to get bytesize
+ * @return result String Size of the input string in bytes
+ */
+function countUtf8(str) {
+    var result = 0;
+    for (var n = 0; n < str.length; n++) {
+        var charCode = fixedCharCodeAt(str, n);
+        if (typeof charCode === "number") {
+            if (charCode < 128) {
+                result = result + 1;
+            } else if (charCode < 2048) {
+                result = result + 2;
+            } else if (charCode < 65536) {
+                result = result + 3;
+            } else if (charCode < 2097152) {
+                result = result + 4;
+            } else if (charCode < 67108864) {
+                result = result + 5;
+            } else {
+                result = result + 6;
+            }
+        }
+    }
+    return result;
+}
+
 function get_logfile_offset(announcer, url) {
     //curl -sI http://crawl.akrasiac.org/milestones-git | grep Content-Length  | awk '{print $2}'
     var child = exec("curl -sI "+url+" | grep Content-Length  | awk '{print $2}'");
@@ -96,8 +162,9 @@ function get_server_logs(announcer) {
                 if (data.search("416 Requested Range Not Satisfiable")==-1) {
                     //console.log(announcer+': ' + data);
                     //console.log(data.replace(/^\s+|\s+$/g, '').split("\n").length+" milestones for "+announcer);
+                    datalength = countUtf8(data);
+                    if (datalength != byteCount(data)) console.log("differing byte counts: old: "+byteCount(data)+", new: "+datalength);
                     data.replace(/^\s+|\s+$/g, '').split(/\n(?=v=)/).forEach(process_milestone);
-                    datalength = byteCount(data);
                     console.log(announcer+' data size: '+datalength+' bytes');
                     //console.log(data);
                     //offset+=datalength;
