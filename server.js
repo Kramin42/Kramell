16064deecaabc4b75c491bc29809a1f0fb0ded39
Cameron <kramin42@gmail.com>
2014-12-06 01:25:25 +1300

    fix week command

--- server.js
+++ server.js
@@ -1446,7 +1446,7 @@ function handle_message(nick, chan, message) {
         if (arg[0]=="info" || arg[0]=="week") {
         	regex = arg.length>1 ? new RegExp(arg.slice(1,arg.length).join(' '),"i") : /.*/;
         	//console.log(arg.length>1 ? arg.slice(1,arg.length).join(' ') : "default");
-			db.csdc.find({"week": regex, "start": {$lte: getTimeStamp()}, "active": true}, {"week": 1, "start": 1, "char": 1, "gods": 1, "bonustext": 1}).sort({"start":-1}).limit(1).toArray().then(function(err, weeks) {
+			db.csdc.find({"week": regex, "start": {$lte: getTimeStamp()}, "active": true}, {"week": 1, "start": 1, "char": 1, "gods": 1, "bonustext": 1}).sort({"start":-1}).limit(1).toArray().then(function(weeks) {
 				week = weeks[0];
 				if (week) {
 					//bot.say(chan, irc.colors.wrap('magenta', "CSDC "+week["week"]);
