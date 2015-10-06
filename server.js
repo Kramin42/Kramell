17fc8493e8e07cfcc4aa26d6b8d20e168f598643
Cameron Dykstra <dykstra.cameron@gmail.com>
2015-10-07 00:17:46 +1300

    increase fetch max size

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ var control_channel = "##kramell";
 var forbidden = ['##crawl','##crawl-dev','##crawl-sequell'];
 
 var csdcrunning = true;
-var fetchlimit = 4095;
+var fetchlimit = 400000;
 
 var timers = {};
 var NAnick;
@@ -226,7 +226,7 @@ function get_logfile_offset(announcer, url) {
 }
 
 function get_server_logs(announcer) {
-	var delay = 10;
+	var delay = 30;
 // 	if (fetching[announcer]) {//don't want simultaneous fetches breaking things
 //     	console.log("preventing simultaneous fetch for "+announcer);
 //     	if (timers[announcer]) {
