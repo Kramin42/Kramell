6aa964dd53b7b4fca33d7f16e5bd150dbbdc6373
Cameron Dykstra <dykstra.cameron@gmail.com>
2015-10-12 12:02:30 +1300

    Add special case for Gozag as csdc god

--- server.js
+++ server.js
@@ -519,7 +519,7 @@ function check_csdc_points(name, milestone, week) {
     
     //4   Champion a listed god (from weekly list):
     //console.log(new RegExp("Champion of ("+week["gods"]+")")); // <= correct
-    if (milestone.search(/type=god.maxpiety/)>-1 && milestone.search(new RegExp("god=("+week["gods"]+")","i"))>-1){
+    if (milestone.search(/type=god.maxpiety|god=Gozag:.*type=god.worship:/)>-1 && milestone.search(new RegExp("god=("+week["gods"]+")","i"))>-1){
         if (points[3]==0 && !player["godabandon"]){
             bot.say('##csdc', irc.colors.wrap('dark_green', name+' (L'+xl+' '+ch+') championed a weekly god ('+god+') for 1 point!'));
             db.csdc.update({"week":week["week"], "players.name":name.toLowerCase()},{$set: {"players.$.points.3":1}});
@@ -527,10 +527,10 @@ function check_csdc_points(name, milestone, week) {
     }
     // if they abandon then they lose the point, ((also it must be their first god to get the point)) - not anymore
     if (milestone.search(/type=god.renounce/)>-1){
-        //if (points[3]==1){
-        //    bot.say('##csdc', irc.colors.wrap('dark_red', name+' (L'+xl+' '+ch+') abandoned a weekly god ('+god+') and lost their point for championing'));
-        //    db.csdc.update({"week":week["week"], "players.name":name.toLowerCase()},{$set: {"players.$.points.3":0}});
-        //}
+        if (points[3]==1 && milestone.search(/god=Gozag/)>-1){
+            bot.say('##csdc', irc.colors.wrap('dark_red', name+' (L'+xl+' '+ch+') abandoned a weekly god ('+god+') and lost their point for championing'));
+            db.csdc.update({"week":week["week"], "players.name":name.toLowerCase()},{$set: {"players.$.points.3":0}});
+        }
         db.csdc.update({"week":week["week"], "players.name":name.toLowerCase()},{$set: {"players.$.godabandon":true}});
     }
     
