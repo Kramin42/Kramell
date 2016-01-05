99bbcd835415e782aa50732cfad2e7b37503c407
Cameron Dykstra <dykstra.cameron@gmail.com>
2016-01-05 17:33:36 +0100

    disable connecting to irc

--- server.js
+++ server.js
@@ -243,7 +243,7 @@ function get_logfile_offset(announcer, url) {
 }
 
 function get_server_logs(announcer) {
-    var delay = 30;
+    var delay = 60;
     //  if (fetching[announcer]) {//don't want simultaneous fetches breaking things
     //      console.log("preventing simultaneous fetch for "+announcer);
     //      if (timers[announcer]) {
@@ -2935,7 +2935,7 @@ function connect() {
     });
 }
 
-connect();
+//connect();
 
 //end IRC bot
 
