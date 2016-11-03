9badece51d4f9f130a79eb2d659df70c393cbba5
Cameron Dykstra <dykstra.cameron@gmail.com>
2016-11-03 20:34:50 +1300

    check logs every 10 seconds

--- server.js
+++ server.js
@@ -244,7 +244,7 @@ function get_logfile_offset(announcer, url) {
 }
 
 function get_server_logs(announcer) {
-    var delay = 60;
+    var delay = 10;
     //  if (fetching[announcer]) {//don't want simultaneous fetches breaking things
     //      console.log("preventing simultaneous fetch for "+announcer);
     //      if (timers[announcer]) {
